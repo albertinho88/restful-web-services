ab9ec2588f63157f660bb32add54c8f8d732246d
Jlarragamena <jlarragamena@trueblue.com>
2024-07-01 22:24:08 -0500

    Initial commit

---
+++ src/test/java/ec/clicka/rest/webservices/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.clicka.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
