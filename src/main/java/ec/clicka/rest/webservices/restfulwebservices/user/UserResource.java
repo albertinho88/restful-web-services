82a66306444a2e0abb7457b991bc80e1a5ea4468
Jlarragamena <jlarragamena@trueblue.com>
2024-07-02 12:00:24 -0500

    Implementing GET methods for User Resource

---
+++ src/main/java/ec/clicka/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,29 @@
+package ec.clicka.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        super();
+        this.service = service;
+    }
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUser(@PathVariable Integer id) {
+        return service.findOne(id);
+    }
+
+}
