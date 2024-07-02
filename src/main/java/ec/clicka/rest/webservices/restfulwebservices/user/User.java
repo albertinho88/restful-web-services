c13b0fee1e0f4a2fd52d898470186125d12c52b7
Jlarragamena <jlarragamena@trueblue.com>
2024-07-02 11:52:22 -0500

    Creating User Bean and UserDaoService

---
+++ src/main/java/ec/clicka/rest/webservices/restfulwebservices/user/User.java
@@ -0,0 +1,20 @@
+package ec.clicka.rest.webservices.restfulwebservices.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+    private Integer id;
+    private String name;
+    private LocalDate birthDate;
+
+}
