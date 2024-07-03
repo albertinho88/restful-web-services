e1f05de55dc6b3c8e5ddcb918bd7b8b546051141
Jlarragamena <jlarragamena@trueblue.com>
2024-07-03 15:22:21 -0500

    Implementing Static Filtering for REST API

---
+++ src/main/java/ec/clicka/rest/webservices/restfulwebservices/user/filtering/SomeBean.java
@@ -0,0 +1,28 @@
+package ec.clicka.rest.webservices.restfulwebservices.user.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@ToString
+//@JsonIgnoreProperties({"field1","field2"})
+public class SomeBean {
+
+    private String field1;
+
+    @JsonIgnore
+    private String field2;
+
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        super();
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+}
