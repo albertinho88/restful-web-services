298b79da611fc52d1bbd0ad1882e785d6612e656
Jlarragamena <jlarragamena@trueblue.com>
2024-07-03 16:56:40 -0500

    Create User Entity and some test data

--- src/main/java/ec/clicka/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/ec/clicka/rest/webservices/restfulwebservices/user/User.java
@@ -1,6 +1,9 @@
 package ec.clicka.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,8 +17,11 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity(name = "user_details")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id, birth_date, name) values (1, current_date(), 'Ranga');
+insert into user_details(id, birth_date, name) values (2, current_date(), 'Ravi');
+insert into user_details(id, birth_date, name) values (3, current_date(), 'Sathish');
